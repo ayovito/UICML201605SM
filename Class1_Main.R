bd50e7f580b20e4a92164c3fc0279a91acf2e18b
Sam Mourad <Sam.Mourad@crowehorwath.com>
2016-05-17 12:22:18 -0500

    Initial code

--- Class1_Main.R
+++ Class1_Main.R
@@ -1,3 +1,6 @@
+#Clear env
+rm(list=ls())
+
 #Create iris promise object
 data("iris")
 
@@ -12,11 +15,67 @@ trainNvalid = union(trainIdx, validIdx)
 setdiff(trainNvalid, as.integer(rownames(iris)))
 
 #clean up environment
-rm(x, y, trainNvalid)
+rm(trainNvalid)
 
-#Train LDA
+#Train LDA####
 library(MASS)
 #ldaFrmla = formula(paste0("trainData$", targetVar, "~."))
 ldaMod = lda(formula = trainData$Species~., data = trainData)
-pred = predict(object = ldaMod, newdata = validData)
-View(pred)
+ldaPred = predict(object = ldaMod, newdata = validData)
+#str(ldaPred)
+#nrow(ldaPred$posterior)
+#ncol(ldaPred$posterior)
+#View(ldaPred)
+
+validData$ldaPredClass = ldaPred$class
+validData$ldaPredProb = apply(ldaPred$posterior, 1, max)
+#View(validData)
+
+#Performance measures
+ldaCntAll = length(validData$Species)
+ldaCntIncorrect = length(which(validData$Species != validData$ldaPredClass))
+ldaCntCorrect = length(which(validData$Species == validData$ldaPredClass))
+ldaErrRate = ldaCntIncorrect / ldaCntAll
+ldaPredRate = (ldaCntCorrect + ldaCntIncorrect) / ldaCntAll
+
+#Display lda results
+sprintf("LDA results: Prediction Rate: %s%% , Error Rate: %s%%",
+        format(ldaPredRate * 100, digits = 4),
+        format(ldaErrRate * 100, digits = 4))
+
+
+#Train QDA####
+qdaMod = qda(formula = trainData$Species~., data = trainData)
+qdaPred = predict(object = qdaMod, newdata = validData)
+#str(qdaPred)
+#nrow(qdaPred$posterior)
+#ncol(qdaPred$posterior)
+#View(qdaPred)
+
+validData$qdaPredClass = qdaPred$class
+validData$qdaPredProb = apply(qdaPred$posterior, 1, max)
+#View(validData)
+
+#Performance measures
+qdaCntAll = length(validData$Species)
+qdaCntIncorrect = length(which(validData$Species != validData$qdaPredClass))
+qdaCntCorrect = length(which(validData$Species == validData$qdaPredClass))
+qdaErrRate = qdaCntIncorrect / qdaCntAll
+qdaPredRate = (qdaCntCorrect + qdaCntIncorrect) / qdaCntAll
+
+#Display qda results
+sprintf("QDA results: Prediction Rate: %s%% , Error Rate: %s%%",
+        format(qdaPredRate * 100, digits = 4),
+        format(qdaErrRate * 100, digits = 4))
+
+
+
+
+
+
+
+
+
+
+
+
